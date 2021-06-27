34299fb3796ed84c086c7f8462b5c7788c15457a
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-06-27 03:42:28 +0100

    add screen structure

---
+++ src/screens/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const Main = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Main Page</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Main;
