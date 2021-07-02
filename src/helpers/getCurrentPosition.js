b0d9e835c64998843fd574fe9cd19a8bdb139e30
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-07-02 23:39:46 +0100

    add getCurrentPosition helper func

--- src/helpers/getCurrentPosition.js
+++ src/helpers/getCurrentPosition.js
@@ -1,23 +1,19 @@
 import Geolocation from 'react-native-geolocation-service';
 
-const getCurrentPosition = async () => {
-  let currentPosition;
-  Geolocation.requestAuthorization('whenInUse').then(r => {
-    if (r === 'granted') {
-      Geolocation.getCurrentPosition(
-        position => {
-          console.log(position);
-          currentPosition = position;
-        },
-        error => {
-          // See error code charts below.
-          console.log(error.code, error.message);
-        },
-        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-      );
-    }
-  });
-  return currentPosition;
+const getCurrentPosition = async setCurrentPosition => {
+  const authorization = await Geolocation.requestAuthorization('whenInUse');
+  if (authorization === 'granted') {
+    Geolocation.getCurrentPosition(
+      position => {
+        setCurrentPosition(position);
+      },
+      error => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }
 };
 
 export default getCurrentPosition;
