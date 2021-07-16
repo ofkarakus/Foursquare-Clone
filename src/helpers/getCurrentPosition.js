36b05ca7c6584abba78495901ac1724bd41526ad
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-07-16 23:35:27 +0100

    review getCurrentPosition func for Android OS

--- src/helpers/getCurrentPosition.js
+++ src/helpers/getCurrentPosition.js
@@ -1,14 +1,30 @@
 import Geolocation from 'react-native-geolocation-service';
+import device from '../constants/device';
 
 const getCurrentPosition = async setCurrentPosition => {
-  const authorization = await Geolocation.requestAuthorization('whenInUse');
-  if (authorization === 'granted') {
+  if (device.ios) {
+    try {
+      const authorization = await Geolocation.requestAuthorization('whenInUse');
+      if (authorization === 'granted') {
+        Geolocation.getCurrentPosition(
+          position => {
+            setCurrentPosition(position);
+          },
+          error => {
+            console.log(error.code, error.message);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (device.android) {
     Geolocation.getCurrentPosition(
       position => {
         setCurrentPosition(position);
       },
       error => {
-        // See error code charts below.
         console.log(error.code, error.message);
       },
       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
