3ebbf36472ed5912f7380fe8da275e973006fb93
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-07-19 03:42:42 +0100

    add Compass icon

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,11 +1,12 @@
-export {default as Breakfast} from './Breakfast';
-export {default as CoffeeTea} from './CoffeeTea';
-export {default as Dinner} from './Dinner';
-export {default as History} from './History';
-export {default as LeftArrow} from './LeftArrow';
-export {default as Lists} from './Lists';
-export {default as Lunch} from './Lunch';
-export {default as NightLife} from './NightLife';
-export {default as Profile} from './Profile';
-export {default as Search} from './Search';
-export {default as ThingsToDo} from './ThingsToDo';
+export { default as Breakfast } from './Breakfast'
+export { default as CoffeeTea } from './CoffeeTea'
+export { default as Compass } from './Compass'
+export { default as Dinner } from './Dinner'
+export { default as History } from './History'
+export { default as LeftArrow } from './LeftArrow'
+export { default as Lists } from './Lists'
+export { default as Lunch } from './Lunch'
+export { default as NightLife } from './NightLife'
+export { default as Profile } from './Profile'
+export { default as Search } from './Search'
+export { default as ThingsToDo } from './ThingsToDo'
